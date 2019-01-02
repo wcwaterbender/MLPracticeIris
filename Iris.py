b5dc476b8d4269cfce4ea4c1c8840151e54e45e3
wcwaterbender <samcornish96@gmail.com>
2019-01-02 11:52:15 -0800

    built and eval'd six different models

--- Iris.py
+++ Iris.py
@@ -34,7 +34,30 @@ array = dataset.values
 X = array[:,0:4]
 Y = array[:,4]
 validation_size =0.20
-seed = 8
+seed = 7
 X_train, X_validation, Y_train, Y_validation = model_selection.train_test_split(X,Y,test_size=validation_size, random_state = seed)
 
+#test harness
+seed = 7
+scoring = 'accuracy'
 
+#building and testing a few models
+
+models = []
+models.append(('LR', LogisticRegression(solver ='liblinear',multi_class='auto')))
+models.append(('LDA', LinearDiscriminantAnalysis()))
+models.append(('KNN', KNeighborsClassifier()))
+models.append(('CART', DecisionTreeClassifier()))
+models.append(('NB', GaussianNB()))
+models.append(('SVM', SVC(gamma = 'auto')))
+
+#evaluate each model
+results = []
+names = []
+for name, model in models:
+    kfold = model_selection.KFold(n_splits=10, random_state=seed)
+    cv_results = model_selection.cross_val_score(model, X_train, Y_train, cv=kfold, scoring=scoring)
+    results.append(cv_results)
+    names.append(name)
+    msg = "%s: %f (%f)" % (name,cv_results.mean(), cv_results.std())
+    print(msg)
